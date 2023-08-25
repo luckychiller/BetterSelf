ced58479e4ebbb793b84e96a41582e1675a8323a
luckychiller <wasswalutufi@gmail.com>
2023-08-25 18:00:14 +0600

    test commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.example.betterself {
     requires javafx.controls;
     requires javafx.fxml;
-        requires javafx.web;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires net.synedra.validatorfx;
-            requires org.kordamp.ikonli.javafx;
-            requires org.kordamp.bootstrapfx.core;
-            requires eu.hansolo.tilesfx;
-        
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
     opens com.example.betterself to javafx.fxml;
     exports com.example.betterself;
 }
