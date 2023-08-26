2a7e3b97e79cd6a497cc5e09f8ce8f9bc864c965
luckychiller <wasswalutufi@gmail.com>
2023-08-26 15:24:29 +0600

    started on email verification ----> not complete present error:no Error

--- src/main/java/com/example/betterself/SignUpController.java
+++ src/main/java/com/example/betterself/SignUpController.java
@@ -10,6 +10,11 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class SignUpController {
     private Stage stage;
@@ -37,6 +42,41 @@ public class SignUpController {
     private Hyperlink LogIn;
     @FXML
     private Hyperlink Help;
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent event) throws IOException {
+        Random random = new Random();
+        int verificationCode = 100000 + random.nextInt(900000);
+        final String senderEmail = "wasswalutufi@iut-dhaka.edu";
+        final String senderPassword = "*******";
+        String recipientEmail=SignUpEmail.getText();
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, senderPassword);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Verification Code");
+            message.setText("Your verification code is: " + verificationCode);
+
+            Transport.send(message);
+            System.out.println("Verification code sent successfully.");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        // ask user to confirm the email. and verify code.
+    }
     @FXML
     protected void onLogInLinkClick(ActionEvent event) throws IOException {
         root= FXMLLoader.load(getClass().getResource("hello-view.fxml"));
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.betterself {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires javax.mail;
 
     opens com.example.betterself to javafx.fxml;
     exports com.example.betterself;
